29f89dcd4b99706ae63e900b6be7344c24792032
Vladimir Proshin <VlaShin1986@gmail.com>
2023-05-14 22:33:20 +0300

    settings changes

---
+++ src/main/java/com/gmail/vladimirprocean/moviegalleryapp/MovieGalleryAppApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.vladimirprocean.moviegalleryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieGalleryAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieGalleryAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gmail/vladimirprocean/moviegalleryapp/MovieGalleryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.vladimirprocean.moviegalleryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieGalleryAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
