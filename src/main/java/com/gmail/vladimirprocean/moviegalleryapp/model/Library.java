c0babccc855629d085e204a7c04a865bfdc2f474
Vladimir Proshin <VlaShin1986@gmail.com>
2023-05-14 22:16:35 +0300

    added Library class

---
+++ src/main/java/com/gmail/vladimirprocean/moviegalleryapp/model/Library.java
@@ -0,0 +1,22 @@
+package com.gmail.vladimirprocean.moviegalleryapp.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "libraries")
+public class Library {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne(mappedBy = "library")
+    private User user;
+
+    public Library() {
+    }
+
+    @ManyToMany
+    @JoinTable(name="libraries_movies", joinColumns = @JoinColumn(name="libraries_id"), inverseJoinColumns = @JoinColumn(name = "movies_id"))
+    private List<Movie> movies;
+}
