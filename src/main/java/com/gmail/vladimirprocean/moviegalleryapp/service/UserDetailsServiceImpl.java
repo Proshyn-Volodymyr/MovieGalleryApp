16ff5b362b150e407a35f2cfa1b8112dbb4a4d38
Vladimir Proshin <VlaShin1986@gmail.com>
2023-05-15 22:15:09 +0300

    Added userRepository to serviceImpl class

--- src/main/java/com/gmail/vladimirprocean/moviegalleryapp/service/UserDetailsServiceImpl.java
+++ src/main/java/com/gmail/vladimirprocean/moviegalleryapp/service/UserDetailsServiceImpl.java
@@ -1,27 +1,36 @@
 package com.gmail.vladimirprocean.moviegalleryapp.service;
 
 import com.gmail.vladimirprocean.moviegalleryapp.model.User;
+import com.gmail.vladimirprocean.moviegalleryapp.repository.UserRepository;
 import com.gmail.vladimirprocean.moviegalleryapp.util.CustomPasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private CustomPasswordEncoder customPasswordEncoder;
+    private final UserRepository userRepository;
 
-    public UserDetailsServiceImpl() {
+    public UserDetailsServiceImpl(UserRepository userRepository) {
         this.customPasswordEncoder = new CustomPasswordEncoder();
+        this.userRepository = userRepository;
     }
 
     //    Talking with the repository to know does our user exist
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = new User();
-        user.setUserName(username);
-        user.setPassword(customPasswordEncoder.getPasswordEncoder().encode("password"));
-        user.setId(1L);
-        return user;
+        Optional<User> userOptional = userRepository.findUserByUsername(username);
+        return userOptional.orElseThrow(() -> new UsernameNotFoundException("Invalid credentials"));
+
+
+//        User user = new User();
+//        user.setUserName(username);
+//        user.setPassword(customPasswordEncoder.getPasswordEncoder().encode("password"));
+//        user.setId(1L);
+//        return user;
     }
 }
