f57650270c61f1e08aba99e980ff1ce4fe894aa5
Vladimir Proshin <VlaShin1986@gmail.com>
2023-05-15 22:14:02 +0300

    Added UserRepository class

---
+++ src/main/java/com/gmail/vladimirprocean/moviegalleryapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.gmail.vladimirprocean.moviegalleryapp.repository;
+
+import com.gmail.vladimirprocean.moviegalleryapp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findUserByUsername(String username);
+}
